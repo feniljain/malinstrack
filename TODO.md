d8d46febaef7555d864d68b80dd09463da6efa43
feniljain <fkjainco@gmail.com>
2023-10-02 02:01:15 +0530

    feat: add inital cli options support and basic setup

---
+++ TODO.md
@@ -0,0 +1,13 @@
+## Should do:
+ - [] take installer's path and track all files accessed/created/deleted by it
+ - [] take app's path and track all files it accesses/creates/deletes
+ - [] Give an interactive way to uninstall application
+ - [] Test with couple of different installation scripts
+ - [] Test with couple of different application installers
+ - [] Whole thing should have a TUI/GUI
+
+## Constraints:
+ - [] Only linux support initially
+
+## To figure out:
+ - [] How to uniquely idenitfy a certain target ( app or script ) across installation script/usage
---
+++ src/main.rs
@@ -0,0 +1,111 @@
+// Malinstrack: Malicious installation tracker
+
+use std::{
+    fs,
+    path::{self, Path},
+    process::Command,
+};
+
+use clap::{Parser, Subcommand};
+use spinners::{Spinner, Spinners};
+
+#[derive(Clone, Subcommand)]
+enum Actions {
+    /// setup malinstrack program
+    Setup,
+    /// track program
+    Track {
+        /// unique identifier to link different scripts/apps to a single report
+        identifier: String,
+        /// path of script/app to track
+        path: path::PathBuf,
+    },
+    /// view report
+    ViewReport { identifier: String },
+}
+
+#[derive(Parser)]
+#[command(author = "feniljain", version = "0.0.1", about, long_about = None)]
+struct Args {
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    action: Actions,
+}
+
+fn main() -> anyhow::Result<()> {
+    // CLI Options:
+    // - Setup: Provide setup command of this project
+    //     - Make a file to report different apps passed to track cmd
+    //     - Check build deps and build a shared library locally
+    // - Track: Take in a path and track all files accessed/created/deleted by it
+    //     - LD_PRELOAD our shared library and run the path
+    // - View Report: Get comprehensive report for given target
+    //     - Make a TUI/GUI to select what all to delete and make a generic uninstaller
+    //     - Also create a generic KB which *suggests* what files you should ideally not delete or
+    //     should definitely delete
+    //     - Can show report in nice table: https://github.com/zhiburt/tabled
+
+    // Shared Library:
+    // - Make functions to override syscalls
+    // - They see what file is accessed/modified and append it to our file
+    // - Parse app/script's linked libs and add them to our file too
+    // - We run a de-duplication job after exec of given binary completes
+
+    let args = Args::parse();
+
+    // TODO: Set debug mode
+
+    match args.action {
+        Actions::Setup => {
+            println!("Starting setup");
+
+            let mut dir_creation_sp = Spinner::new(
+                Spinners::Dots9,
+                "creating dirs for handling everything related to malinstrack\n".into(),
+            );
+
+            #[allow(deprecated)]
+            let home_dir_path = std::env::home_dir().expect("Expected home dir path");
+            let reports_dir_path = Path::join(&home_dir_path, Path::new(".malinstrack/reports/"));
+            fs::create_dir_all(reports_dir_path)?;
+            dir_creation_sp.stop();
+            println!("finished creating dirs ✅");
+            println!();
+
+            println!("=============================================");
+            println!("finding gcc by asking for it's version, output:");
+            // TODO: Create shared object
+            check_build_deps()?;
+            println!("=============================================");
+            println!();
+
+            println!("setup completed :)");
+            println!("let's track those malicious actors down!");
+        }
+        Actions::Track { path, identifier } => {
+            // fs::write("~/.malinstrack/reports/iden", "")?;
+            println!(
+                "Starting to track identifier: {identifier} at path: {}",
+                path.to_str()
+                    .expect("could not print path, what did you pass bruhh")
+            );
+        }
+        Actions::ViewReport { identifier } => {
+            println!("Generating report for identifier: {identifier}");
+        }
+    }
+
+    Ok(())
+}
+
+fn check_build_deps() -> anyhow::Result<()> {
+    Command::new("gcc")
+        .args(["--version"])
+        .status()
+        .expect("could not find gcc compiler");
+
+    Ok(())
+}
